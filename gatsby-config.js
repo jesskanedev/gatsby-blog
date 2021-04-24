54038b6232a831f4ba7643bef363ede562795b38
Jessica Kane <Jessica.Kane@intergen.co.nz>
2021-04-24 12:58:48 +1200

    Update social links and site title.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `Jess Kane | Development Blog`,
   },
   plugins: [
     {
@@ -25,12 +25,12 @@ module.exports = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/jesskanedev/`,
           },
           {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            name: `Twitter`,
+            url: `https://twitter.com/jesskanedev`,
           },
         ],
       },
